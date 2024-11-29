39def951cbd3a667c5ac649be5aa29212c56f631
Julius De Baaij <jde-baai@student.codam.nl>
2024-11-29 17:52:13 +0100

    buildpath update

--- include/httpHandler.hpp
+++ include/httpHandler.hpp
@@ -6,7 +6,7 @@
 /*   By: jde-baai <jde-baai@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/21 12:33:45 by jde-baai      #+#    #+#                 */
-/*   Updated: 2024/11/29 17:26:15 by jde-baai      ########   odam.nl         */
+/*   Updated: 2024/11/29 17:51:32 by jde-baai      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ private:
 	std::optional<std::string> readFile(std::string &filename);
 	std::string contentType(const std::string &filePath);
 	void setErrorResponse(eHttpStatusCode code, std::string msg);
-	std::string httpHandler::buildPath(void);
+	std::string buildPath(void);
 	// parse
 	void parseRequestLine(std::stringstream &ss);
 	void parseHeaders(std::stringstream &ss);
