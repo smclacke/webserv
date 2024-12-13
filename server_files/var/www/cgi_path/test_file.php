0ce4b2f1c2f95cc2d09ab820fa9137861ecf26d3
Julius de Baaij <julius@debaaij.com>
2024-12-13 15:38:41 +0100

    it works

--- server_files/var/www/cgi_path/test_file.php
+++ server_files/var/www/cgi_path/test_file.php
@@ -1,12 +1,14 @@
-<?php
-// Start the HTML output
-echo "<html>";
-echo "<head>";
-echo "<title>Simple CGI Script</title>";
-echo "</head>";
-echo "<body>";
-echo "<h1>Hello, World!</h1>";
-echo "<p>This is a simple CGI script written in PHP.</p>";
-echo "</body>";
-echo "</html>";
-?>
+#!/usr/bin/php
+   <?php
+   // Send HTTP headers
+   header("Content-Type: text/html");
+   echo "<html>";
+   echo "<head>";
+   echo "<title>Simple CGI Script</title>";
+   echo "</head>";
+   echo "<body>";
+   echo "<h1>Hello, World!</h1>";
+   echo "<p>This is a simple CGI script written in PHP.</p>";
+   echo "</body>";
+   echo "</html>";
+   ?>
