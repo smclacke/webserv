b48931e8adfa52fca2b494588a220f65741170ce
JuliusdB <julius@debaaij.com>
2024-12-31 14:50:03 +0100

    Log file added to server constructor&decon

--- src/server/webserv.cpp
+++ src/server/webserv.cpp
@@ -6,7 +6,7 @@
 /*   By: jde-baai <jde-baai@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/22 15:22:59 by jde-baai      #+#    #+#                 */
-/*   Updated: 2024/12/11 16:44:49 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/12/31 14:27:30 by juliusdebaa   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
